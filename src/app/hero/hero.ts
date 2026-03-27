450b68f89fd967168e65503f1126ca29d4ec3f9f
Adil Qureshi <adilqureshi.dev@gmail.com>
2026-03-27 22:27:33 +0300

    changes

--- src/app/hero/hero.ts
+++ src/app/hero/hero.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Service } from "../service/service";
 import { Project } from "../project/project";
+import { HowItWorks } from "../how-it-works/how-it-works";
+import { Testimonial } from "../testimonial/testimonial";
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [Service, Project],
+  imports: [Service, Project, HowItWorks, Testimonial],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
